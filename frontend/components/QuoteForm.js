b622145ac6a5b9c01d2be9da3de8774e485370a2
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-05-14 18:10:34 -0700

    finished

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,24 +1,51 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, {useReducer} from 'react' // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
-
+const initialState = {
+  authorName: '',
+  quoteText: ''
+}
 // 👇 create your reducer function here
 
-export default function TodoForm({ createQuote = () => { } }) {
-  // 👇 use the reducer hook to spin up state and dispatch
-
-  const onChange = () => {
-    // 👇 implement
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_INPUT': {
+      const { name, value } = action.payload
+      return {...state, [name]: value}
+    }
+    
+    case 'RESET_FORM':
+      return {...state, authorName: '', quoteText: ''}
+    default:
+      return state
   }
+}
+
+export default function TodoForm({ createQuote }) {
+  // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer (reducer, initialState)
+
+  const onChange = (evt) => {
+    // 👇 implement
+    const {value, name} = evt.target
+    dispatch({type: 'CHANGE_INPUT', payload: {value, name}})
+  }
+
   const resetForm = () => {
     // 👇 implement
+    dispatch({type: 'RESET_FORM'})
   }
-  const onNewQuote = () => {
+
+  const onNewQuote = (evt) => {
     // 👇 implement
+    
+    evt.preventDefault()
+    const {authorName, quoteText} = state
+    createQuote({authorName, quoteText})
     resetForm()
   }
 
@@ -30,6 +57,7 @@ export default function TodoForm({ createQuote = () => { } }) {
         <input
           type='text'
           name='authorName'
+          value = {state.authorName}
           placeholder='type author name'
           onChange={onChange}
         />
@@ -38,6 +66,7 @@ export default function TodoForm({ createQuote = () => { } }) {
         <textarea
           type='text'
           name='quoteText'
+          value = {state.quoteText}
           placeholder='type quote'
           onChange={onChange}
         />
